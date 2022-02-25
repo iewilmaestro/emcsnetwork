d851ea79e1c0f2be7dd1a82420fb3ccceb9dc648
iewilmaestro <83399918+iewilmaestro@users.noreply.github.com>
2022-02-25 10:15:36 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -55,24 +55,29 @@ function a($u, $h = 0, $p = 0, $x = 0){
 		return array($hd,$bd);
 	}
 }
-function b(){
-	c();
-	$m = "\033[1;31m";
-	$p = "\033[1;37m";
-	$k = "\033[1;33m";
-	$h = "\033[1;32m";
-	$u = "\033[1;35m";
-	$b = "\033[1;34m";
-	$c = "\033[1;36m";
-	$mp = "\033[101m\033[1;37m";
-	$cl = "\033[0m";
-	$mm = "\033[101m\033[1;31m";
-	$hp = "\033[1;7m";
-	$z = trim(strtoupper(a[1]));
-	$x = 18;
-	$y = strlen($z);
-	$line = str_repeat('_',$x-$y);
-	echo "\n{$m}[{$p}Script{$m}]->{$k}[".$h.$z."{$k}]-[".$h.v.$k."]".$p.$line.".\n{$u}.__              .__.__ 	    {$p}| \n{$u}|__| ______  _  _|__|  |	\n|  |/ __ \ \/ \/ /  |  |\n|  \  ___/\     /|  |  |__\n|__|\___  >\/\_/ |__|____/\n        \/\n{$mm}[{$mp}▶{$mm}]{$cl} {$k}https://www.youtube.com/c/iewil\n{$c}{$hp} >_{$cl}{$b} Team-Function-INDO\n{$p}────────────────────────────────────\nLink Reg : ".$reg."\n\n";
+function b($i=0){
+	if(!$i){
+		c();
+	}
+	$s = file_get_contents(a[4]);
+	$t = e($s,'#'.a[1].':','|',1);
+	if($t == "on"){
+		$st = w("Online","h");
+	}elseif($status == "off" or $status == null){
+		$st = w("Offline","m");
+	}
+	$z = trim(strtolower(a[1]));
+	return p.' Day:'.date("l").'    Date:'.date("d M Y").'    Time:'.date("H:i").n.l().
+h." ╔═╗┬ ┬┌┐┌┌─┐┌┬┐┬┌─┐┌┐┌   ╔═╗┌─┐┌┬┐┬┬ ┬ ┬   ╦╔╦╗".n.
+k." ╠╣ │ │││││   │ ││ ││││───╠╣ ├─┤│││││ └┬┘───║ ║║".n.
+p." ╚  └─┘┘└┘└─┘ ┴ ┴└─┘┘└┘   ╚  ┴ ┴┴ ┴┴┴─┘┴    ╩═╩╝".n.l().
+' Script    : '.$z.' '.v.'          '.$st.n.n.
+'『』Creator: iewil'.n.'『』Youtube: youtube.com/c/iewil'.n.
+'『』support: All-Team-Function & All-Member'.n.
+'『』D_TRX  : TK67fkL9EpcoCqP2kxonwMmmwyQ5pJmm'.n.
+w('『』NOTE   : THIS SCRIPT FREE NOT FOR SALE','u').n.l().n;
+
+g();
 }
 function c(){
 	return system('clear');
@@ -84,8 +89,15 @@ function d($m){
 		usleep(70000);
 	}
 }
+function e($a,$b,$c,$d){
+	return explode($c,explode($b,$a)[$d])[0];
+}
+function g(){
+	$a = w("Script mati karena web update / scam!","m")."\nSupport Channel saya dengan cara\nSubscribe ".w("https://www.youtube.com/c/iewil","k")."\nkarena subscribe itu gratis :D\nUntuk mendapatkan info Script terbaru\nJoin grub via telegram ~> ".w("https://t.me/Iewil_G","c")."\nðŸ‡®ðŸ‡© ".w("Family-Team-Function-INDO","b")."\n";
+	print d($a);
+}
 function l(){
-	$l = 36;
+	$l = 50;
 	return str_repeat('─',$l).n;
 }
 function s($n){
@@ -104,6 +116,14 @@ function t(){
 		date_default_timezone_set($t);
 	}
 }
+function w($s,$c){
+	if($c == "r"){
+		$w = ['h','k','b','u','m'];
+		$c = $w[array_rand($w)];
+	}
+	$w = ['h'=>h,'k'=>k,'b'=>b,'u'=>u,'m'=>m,'p'=>p];
+	return $w[$c].$s.d;
+}
 function x(){
 	error_reporting(0);
 	if(strpos(b(1),"iewil") == ""){
@@ -121,65 +141,68 @@ function h(){
 }
 function auto(){
 	$url = ht.'/dashboard/claim/auto/start';
-	return a($url,h());
+	return a($url,h())[1];
 }
 function api(){
 	$url = ht.'/dashboard/claim/auto/api';
-	return json_decode(a($url,h()),1);
+	return json_decode(a($url,h())[1],1);
 }
 function Credit(){
-	$r = a(host().'/dashboard',h());
+	$r = a(ht.'/dashboard',h())[1];
 	preg_match_all('#<p class="amountText">(.*?)<span class="text-uppercase">(.*?)</span>#is',$r,$coin);
-	echo str_repeat(' ',11).c.'Credit Balance'."\n";
+	echo str_repeat(' ',17).c.'Credit Balance'."\n";
 	for($i=0;$i<count($coin[1]);$i++){
-		if($i % 2 == 0){
-			echo h.trim($coin[1][$i]).' '.k.substr($coin[2][$i],0,3).m."   |   ";
+		if($i % 3 == 1 || $i % 3 == 0){
+			echo h.trim($coin[1][$i]).' '.k.substr($coin[2][$i],0,3).m." | ";
 		}else{
 			echo h.trim($coin[1][$i]).' '.k.substr($coin[2][$i],0,3)."\n";
 		}
 	}
-	l();
+	print l();
 }
 function _f(){
-	auto();
+	gau:auto();
 	while(true){
 		$api = api();
 		$time = $api["seconds"];
 		$msg = $api["message"];
 		auto();
-		if($time){
-			tmr($time);
-		}
+		if($msg=="time"){
+			tmr($time);goto gau;
+		}else
 		if($msg=="no_autoclaims"){
 			echo m.'insufficient BITS Balance'."\n";
 			exit;
+		}else
+		if($msg=="ok"){
+			//api();
+			Credit();
+			$energy = explode('<span>',explode('<p class="amount">',auto())[1])[0];
+			echo h."Energy   ~> ".k.$energy."\n";print l();
+			if($time){tmr($time);}
 		}
-		//api();
-		Credit();
-		$energy = explode('<span>',explode('<p class="amount">',auto())[1])[0];
-		echo h."Energy ~> ".k.$energy."\n";l();
 	}
 
 }
 function _r(){
-	t();b();
+	t();print b();
 	cookie:x();
 	s('Cookie');
 	s('User_Agent');
 	system("termux-open-url ".a[3]);
-	b();
+	print b();
 
 	$r = auto();
 	$user = explode('</p>',explode('<p class="username">',$r)[1])[0];
 	$energy = explode('<span>',explode('<p class="amount">',$r)[1])[0];
 	echo h."Username ~> ".k.$user."\n";
-	echo h."Energy ~> ".k.$energy."\n";
-	l();
+	echo h."Energy   ~> ".k.$energy."\n";
+	print l();
 	menu:
 	echo m."1 >".p." AutoFaucet\n";
 	echo m."2 >".p." Update Cookie\n";
-	$pil = readline(h."Input Number ".m."> ".p));
-	l();
+	$pil = readline(h."Input Number ".m."> ".p);
+	print l();
 	if($pil==1){
 		goto faucet;
 	}elseif($pil==2){
@@ -191,3 +214,15 @@ function _r(){
 	faucet:
 	_f();
 }
+_r();
+function tmr($tmr){
+	$timr=time()+$tmr;
+	while(true){
+		echo "\r                       \r";
+		$res=$timr-time();
+		if($res < 1){break;}
+		echo w(date('i:s',$res),"r");sleep(1);
+	}
+}
+
+
